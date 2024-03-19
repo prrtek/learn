7772d465c29862d724b1d920528828dd9f244d61
prrtek <prrtek@outlook.com>
2024-03-19 22:39:23 +0530

    Complete Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,25 @@
-export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
-}
+import Layout from "./Layout/Layout";
+
+import { Contact } from "./pages/Contact";
+import { createBrowserRouter } from "react-router-dom";
+import { SignIn } from "./pages/Signin";
+import { SignUp } from "./pages/Signup";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/login", element: <SignIn /> },
+      { path: "/register", element: <SignUp /> },
+      { path: "/contact", element: <Contact /> },
+    ],
+    errorElement: <h1>You are Lost</h1>,
+  },
+]);
+
+export default router;
